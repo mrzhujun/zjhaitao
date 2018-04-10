3b9b34509fd568c998c24916c705408761a1d321
mrzhujun <450453892@qq.com>
2018-04-10 09:21:18 +0800

    修改配置文件

--- application/config.php
+++ application/config.php
@@ -55,11 +55,11 @@ return [
     // | 模块设置
     // +----------------------------------------------------------------------
     // 默认模块名
-    'default_module'         => '',
+    'default_module'         => 'admin',
     // 禁止访问模块
     'deny_module_list'       => [''],
     // 默认控制器名
-    'default_controller'     => '',
+    'default_controller'     => 'index',
     // 默认操作名
     'default_action'         => 'index',
     // 默认验证器
