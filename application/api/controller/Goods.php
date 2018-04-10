8b487ba213b0fe6501854e9ef2a4857c38a3949a
mrzhujun <450453892@qq.com>
2018-04-10 17:56:21 +0800

    完成品牌推荐及相似商品推荐

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -30,6 +30,9 @@ class Goods extends Api
     {
         parent::__construct($request);
         $goods_id = $request->param()['goods_id'];
+        if (!$goods_id) {
+            $this->error('参数错误');
+        }
         $goodsObj = MallGoods::where('goods_id',$goods_id)->field('goods_id,goods_name,goods_brief,goods_desc,cat_id,brand_id,shop_price,goods_images,sell_count,is_onsale')->find();
         if (!$goodsObj->is_onsale) {
             $this->error('该商品暂未出售');
@@ -58,5 +61,72 @@ class Goods extends Api
         }
     }
 
+    /**
+     * 相似商品推荐
+     * @ApiParams   (name="goods_id", type="int", required=true, description="商品id")
+     * @ApiReturn   (data="{
+     *     'code':'1/0',
+     *     'msg':'返回成功/失败',
+     *     'time':'1523173262',
+     *     'data': [
+     *          {
+     *           'goods_name': '急救美人 First aid beauty|FAB修复面霜',
+     *           'shop_price': '88.00',
+     *           'goods_images': '/uploads/20180410/cdcc64a5a96c8ff9b3aabe3f02625408.jpg'
+     *           },
+     *           {
+     *           'goods_name': '迪奥 Dior|2018限量真皮完美恒久气垫',
+     *           'shop_price': '436.00',
+     *           'goods_images': '/uploads/20180410/f554e7ff3c6a5c74e19524620d5dde37.jpg'
+     *           }
+     *       ]
+     *     }")
+     */
+    public function recommend_similar()
+    {
+        $list = MallGoods::where('cat_id',$this->goodsObj->cat_id)->where('goods_id','<>',$this->goodsObj->goods_id)->field('goods_name,shop_price,goods_images')->select();
+        if (!$list) {
+            $this->error('没有推荐信息');
+        }
+        foreach ($list as $k => $v){
+            $arr = explode(',',$v->goods_images);
+            $list[$k]->goods_images = $arr[0];
+        }
+        $this->success('返回成功',$list);
+    }
+
+    /**
+     * 品牌推荐
+     * @ApiParams   (name="goods_id", type="int", required=true, description="商品id")
+     * @ApiReturn   (data="{
+     *     'code':'1/0',
+     *     'msg':'返回成功/失败',
+     *     'time':'1523173262',
+     *     'data': [
+     *          {
+     *           'goods_name': '急救美人 First aid beauty|FAB修复面霜',
+     *           'shop_price': '88.00',
+     *           'goods_images': '/uploads/20180410/cdcc64a5a96c8ff9b3aabe3f02625408.jpg'
+     *           },
+     *           {
+     *           'goods_name': '迪奥 Dior|2018限量真皮完美恒久气垫',
+     *           'shop_price': '436.00',
+     *           'goods_images': '/uploads/20180410/f554e7ff3c6a5c74e19524620d5dde37.jpg'
+     *           }
+     *       ]
+     *     }")
+     */
+    public function recommend_brand()
+    {
+        $list = MallGoods::where('brand_id',$this->goodsObj->brand_id)->where('goods_id','<>',$this->goodsObj->goods_id)->field('goods_name,shop_price,goods_images')->select();
+        if (!$list) {
+            $this->error('没有推荐信息');
+        }
+        foreach ($list as $k => $v){
+            $arr = explode(',',$v->goods_images);
+            $list[$k]->goods_images = $arr[0];
+        }
+        $this->success('返回成功',$list);
+    }
     
 }
