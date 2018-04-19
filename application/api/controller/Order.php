a5306757672a23b2a6e823a2d324d4ea327ffda9
mrzhujun <450453892@qq.com>
2018-04-19 14:58:25 +0800

    微信支付及支付回调更改订单状态

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -8,6 +8,7 @@ use app\api\model\MallCouponsUser;
 use app\api\model\MallGoods;
 use app\api\model\MallOrder;
 use app\api\service\Order as ServiceOrder;
+use app\api\service\WeixinPay;
 use app\api\validate\CarToOrder;
 use app\api\validate\ConfirmOrder;
 use think\Db;
@@ -26,6 +27,7 @@ class Order extends Common
      */
     public function order_list()
     {
+
         $userObj = $this->check_user();
         $orderList = MallOrder::where('user_id','=',$userObj->user_id)
             ->with(['order_goodslists'=>function($query){$query->field('order_num,goods_id,goods_name,price,spec_info,image');}])
@@ -173,6 +175,7 @@ class Order extends Common
      */
     public function order_close()
     {
+        echo __DIR__;exit();
         $this->check_user();
         Db::startTrans();
         try{
@@ -184,7 +187,57 @@ class Order extends Common
         }
     }
 
+    /**
+     * post: 订单发起支付
+     * path: pay
+     * param: token - {string} token方法获取
+     * param: order_num - {string} 订单号
+     */
+    public function pay()
+    {
+        $userObj = $this->check_user();
+        $orderDetail = MallOrder::get(input('order_num'));
+        $money = $orderDetail->final_price;
+        $weixinpay = new WeixinPay(config('wx.app_id'), $userObj->open_id, config('wx.mch_id'), config('wx.mch_secret'), $money ,config('wxpay_notify_url') , input('order_num'));
+        $return = $weixinpay -> pay();
+        //返回5个支付参数和sign
+        return json($return);
+    }
 
 
+    /**
+     * 微信支付回调接口
+     */
+    public function paycallback()
+    {
+        $xml = file_get_contents('php://input');
+        // 这句file_put_contents是用来查看服务器返回的XML数据 测试完可以删除了
+        file_put_contents(__DIR__.'/public/statics/log/log.txt',$xml,FILE_APPEND);
 
+        $arr = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+
+        //用户http_build_query()将数据转成URL键值对形式
+        $sign = http_build_query($arr);
+        //md5处理
+        $sign = md5($sign);
+        //转大写
+        $sign = strtoupper($sign);
+        //验签名。默认支持MD5
+        if ( $sign === $arr['sign']) {
+            //获取服务器返回的数据
+            $order_sn = $arr['out_trade_no'];          //订单单号
+            $openid = $arr['openid'];                  //付款人openID
+            $total_fee = $arr['total_fee'];            //付款金额
+            $transaction_id = $arr['transaction_id'];  //微信支付订单号
+        }else{
+            $result = false;
+        }
+        // 返回状态给微信服务器
+        if ($result) {
+            $str='<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
+        }else{
+            $str='<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[签名失败]]></return_msg></xml>';
+        }
+        echo $str;
+    }
 }
---
+++ application/lib/exception/WxPayExcrption.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: zhujun
+ * DateTime: 2018/4/19 14:22
+ */
+
+namespace app\lib\exception;
+
+
+class WxPayExcrption extends BaseException
+{
+
+}
